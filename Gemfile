9e506fa411ff553f39a426f12ac542f2d1396de0
sayginburak <sayginburak17@gmail.com>
2015-09-26 13:29:00 +0300

    heroku configs

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 gem 'bootstrap-sass', '~> 3.2.0'
@@ -32,6 +32,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Unicorn as the app server
 # gem 'unicorn'
+gem 'rails_12factor', group: :production
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -46,4 +47,6 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
+
+ruby "2.2.0"
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150925083159) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "favs", force: :cascade do |t|
     t.integer  "project_id"
     t.integer  "money"
@@ -21,7 +24,18 @@ ActiveRecord::Schema.define(version: 20150925083159) do
     t.integer  "investment_id"
   end
 
-  add_index "favs", ["investment_id"], name: "index_favs_on_investment_id"
+  add_index "favs", ["investment_id"], name: "index_favs_on_investment_id", using: :btree
+
+  create_table "feeds", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "link",       limit: 255
+    t.string   "feed",       limit: 255
+    t.integer  "planet_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "feeds", ["planet_id"], name: "index_feeds_on_planet_id", using: :btree
 
   create_table "investments", force: :cascade do |t|
     t.integer  "user_id"
@@ -29,13 +43,71 @@ ActiveRecord::Schema.define(version: 20150925083159) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "investments", ["user_id"], name: "index_investments_on_user_id"
+  add_index "investments", ["user_id"], name: "index_investments_on_user_id", using: :btree
+
+  create_table "options", force: :cascade do |t|
+    t.text     "text"
+    t.text     "point"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "options", ["question_id"], name: "index_options_on_question_id", using: :btree
+
+  create_table "planets", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "subdomain",  limit: 255
+    t.string   "template",   limit: 255
+  end
+
+  create_table "polyclinics", force: :cascade do |t|
+    t.text     "name"
+    t.integer  "symptom_id"
+    t.integer  "patient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "polyclinics", ["patient_id"], name: "index_polyclinics_on_patient_id", using: :btree
+  add_index "polyclinics", ["symptom_id"], name: "index_polyclinics_on_symptom_id", using: :btree
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "code"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text     "name"
+    t.integer  "symptom_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "questions", ["symptom_id"], name: "index_questions_on_symptom_id", using: :btree
+
+  create_table "search_suggestions", force: :cascade do |t|
+    t.string   "term"
+    t.integer  "popularity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "symptoms", force: :cascade do |t|
+    t.text     "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +120,9 @@ ActiveRecord::Schema.define(version: 20150925083159) do
     t.datetime "updated_at",   null: false
   end
 
+  add_foreign_key "favs", "investments"
+  add_foreign_key "investments", "users"
+  add_foreign_key "options", "questions"
+  add_foreign_key "polyclinics", "symptoms"
+  add_foreign_key "questions", "symptoms"
 end
